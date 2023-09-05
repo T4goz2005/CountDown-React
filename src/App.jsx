f4e9747703236cba96b4d69a96e16f59a8eda8da
T4goz2005 <thiagopedrero83@gmail.com>
2023-09-05 18:59:26 -0300

    Fist commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+import useCoutdown from "./hooks/useCountdown";
+
+import imgFundo from "./img/ftfundo.jpg"
+
+function App() { 
+
+  const [day, hour, minute, second] = useCoutdown("dec 19, 2023 00:00:00")
+
+  return (
+    <div className="App" style={{backgroundImage: `url(${imgFundo})`}}>
+      <div className="container">
+        <Title title="Proximo Aniversário da Deh... "/>
+
+        <div className="countdown-container">
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/> 
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
